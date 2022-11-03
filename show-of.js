1ccf93d6300d85d827be1b49254475382aedbc52
Miriam Asadu <miriam.odenyi@gmail.com>
2022-11-03 20:16:23 +0100

    used the for of loop on an array

--- show-of.js
+++ show-of.js
@@ -1,11 +1,13 @@
 function showWithForLoop(array) {
  //Complete your implementation here
-
+ for (const currentElement of array) {
+     console.log(currentElement);
+ }
 }
 
 
 
 
-const sampleArray = [ 1, 2, 3, 4, 5 ];
+const sampleArray = [ 1, 2, 309, 4, 5 ];
 showWithForLoop(sampleArray);
 
