ab8eb0c57c46b6c8579445345c5d0d418ec466ee
Miriam Asadu <miriam.odenyi@gmail.com>
2022-11-04 14:33:37 +0100

    worked on function findItem

--- find.js
+++ find.js
@@ -1,9 +1,22 @@
 function findItem(array, searchItem) {
- //Complete Implementation
+    //Complete Implementation
 
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] === searchItem) {
+           return "YES";
+        }
+    }
+   
+    return "NO";
 }
 
-const item = 4;
-const sampleArray = [ 1, 2, 3, 4, 5];
-findItem(sampleArray, item);
+const item = 5;
+const sampleArray = [1, 2, 3, 4, 5];
+
+let result = findItem(sampleArray, item);
+console.log(result);
+result = findItem(sampleArray, 1);
+console.log(result);
+result = findItem(sampleArray, 6);
+console.log(result);
 
